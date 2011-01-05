b8bf03d3253c5c8bf98821cfce811a79f9516e7c
Teddy Zhai <tzhai@liacs.nl>
2011-01-05 10:27:46 +0000

    *** empty log message ***

---
+++ global.h
@@ -0,0 +1,14 @@
+/*
+ * global.h
+ *
+ *  	Created on: Jan 5, 2011
+ *      Author: Teddy Zhai
+ *      $Id: global.h,v 1.1 2011/01/05 10:27:46 tzhai Exp $
+ */
+
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+/* global options*/
+
+#endif /* GLOBAL_H_ */
