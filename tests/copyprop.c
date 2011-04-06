2f1b6ea4d4213bba99c051dd4db5b67b1fee7856
Teddy Zhai <tzhai@liacs.nl>
2011-04-06 13:13:25 +0000

    - copy propagation for array does not work

--- tests/copyprop.c
+++ tests/copyprop.c
@@ -7,7 +7,7 @@ void trans(int s1, int s2, int *d);
 void sink(int v);
 
 int main() {
-  int a,b;
+  int a[M],b[M];
   int v[M],w[M];
   int x[M],y[M];
   int z[M];
@@ -20,16 +20,16 @@ int main() {
 
   for (i = 0; i < M; i++) {
     if (i < M/2) {
-      a = v[i];
-      b = x[i];
+      a[i] = v[i];
+      b[i] = x[i];
     }
     else {
-      a = w[i];
-      negate( y[i], &b );
+      a[i] = w[i];
+      negate( y[i], &b[i] );
     }
     // a = v[i] or w[i]     propagation works fine
     // b = x[i] or -y[i]    propagation breaks, caused by the negate function call
-    trans( a, b, &(z[i]) );
+    trans( a[i], b[i], &(z[i]) );
   }
 
   for (i = 0; i < M; i++) {
