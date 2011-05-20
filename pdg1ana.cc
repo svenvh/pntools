0408ac796caabb5e5f8ea918cea0b457bc7a14ec
Sven van Haastregt <svhaastr@liacs.nl>
2011-05-20 10:08:58 +0000

    Add edge statistics to pdg1ana

--- pdg1ana.cc
+++ pdg1ana.cc
@@ -2,7 +2,7 @@
 //
 //
 //
-// Analyze PDG1 (PDG without dep info)
+// Analyze PDG1
 //
 //
 //
@@ -37,20 +37,40 @@ int main(int argc, char * argv[])
     exit(1);
   }
 
-  if (pdg->dependences.size() != 0) {
+  /*if (pdg->dependences.size() != 0) {
     fprintf(stderr, "Error: input already contains dependence information.\n");
     exit(1);
+  }*/
+
+  int j;
+  int nMult = 0, nReorder = 0;
+  for (j = 0; j < pdg->dependences.size(); j++) {
+    pdg::dependence *edge = pdg->dependences[j];
+    if (edge->multiplicity) {
+      nMult++;
+    }
+    if (edge->reordering) {
+      nReorder++;
+    }
   }
+  printf("Nodes: %d\n", pdg->nodes.size());
+  printf("Edges: %d of which:\n", pdg->dependences.size());
+  printf("       %d multiplicity\n", nMult);
+  printf("       %d reordering\n", nReorder);
+  printf("\n");
 
   int i;
   for (i = 0; i < pdg->nodes.size(); i++) {
     pdg::node *node = pdg->nodes[i];
-    printf("Node: %2d  line %3d %30s  pre:", node->nr, node->statement->line, node->statement->top_function->name->s.c_str());
+    printf("Node: %2d  line %3d  dim %d  %s", node->nr, node->statement->line, node->source->dim, node->statement->top_function->name->s.c_str());
+    // To print the prefix:
+    /*printf("Node: %2d  line %3d %30s  pre:", node->nr, node->statement->line, node->statement->top_function->name->s.c_str());
     for (int j = 0; j < node->prefix.size(); j++) {
       printf("%s%2d", j==0?"":", ", node->prefix[j]);
-    }
+    }*/
     printf("\n");
   }
+
 
   //pdg->add_history_line("pdg1ana", argc, argv);
   //pdg->Dump(out);
